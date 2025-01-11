9e2f6cbafdc270021ad0d8100ff000ca4152edfa
Juan Lima <juan_jpl@hotmail.com>
2025-01-10 23:31:12 -0300

    fix

---
+++ js/presentation.js
@@ -0,0 +1,13 @@
+window.onload = function(e){ 
+    console.log("Bienvenidos")
+  
+    setTimeout(() => {
+        const caja = document.getElementById("box")
+        caja.classList.add("box-motion")
+          }, "3500");
+
+    
+          setTimeout(()=>{window.location = '../pages/pageOne.html' },4500)
+       
+}
+    
